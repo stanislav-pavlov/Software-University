bba309edef175268d27bf4f92f72322753a904c0
stanislav-pavlov <stanislav.pavlov.94@gmail.com>
2022-08-28 14:37:46 +0300

    Python Fundamentals task

---
+++ Python-Fundamentals/03.Functions/Exercise/e01_smallest_of_three.py
@@ -0,0 +1,9 @@
+def smallest_number(a, b, c):
+    s_num = min(a, b, c)
+    return s_num
+
+
+first_num = int(input())
+second_num = int(input())
+third_num = int(input())
+print(smallest_number(first_num, second_num, third_num))
