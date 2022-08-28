def99381a9208c55e67b37a788e956bc6d0cd88b
stanislav-pavlov <stanislav.pavlov.94@gmail.com>
2022-08-28 14:38:27 +0300

    Python Fundamentals task

---
+++ Python-Fundamentals/03.Functions/Exercise/e02_add_n_subtract.py
@@ -0,0 +1,16 @@
+def sum_numbers(a, b):
+    return a + b
+
+
+def subtract(current_sum, c):
+    return sum_numbers(first, second) - c
+
+
+first = int(input())
+second = int(input())
+third = int(input())
+
+print(subtract(sum_numbers(first, second), third))
+
+# print(sum_numbers(a, b))
+# print(subtract(sum_numbers(a, b), c))
