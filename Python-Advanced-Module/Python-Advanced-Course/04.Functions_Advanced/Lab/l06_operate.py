5320bc65ccb646abcb1d7511604a130f7c8aaf2a
stanislav-pavlov <stanislav.pavlov.94@gmail.com>
2022-11-26 09:50:52 +0200

    Python Fundamentals task

---
+++ Python-Advanced-Module/Python-Advanced-Course/04.Functions_Advanced/Lab/l06_operate.py
@@ -0,0 +1,18 @@
+from functools import reduce
+
+
+def operate(operator, *args):
+    if operator == "+":
+        return reduce(lambda a, b: a+b, args)
+
+    elif operator == "-":
+        return reduce(lambda a, b: a-b, args)
+
+    elif operator == "*":
+        return reduce(lambda a, b: a*b, args)
+
+    elif operator == "/":
+        return reduce(lambda a, b: a/b, args)
+
+
+print(operate("/", 8, 4))
