9916cc01caa008a8b042b856d212ba6f8f2dac59
stanislav-pavlov <stanislav.pavlov.94@gmail.com>
2023-01-11 14:02:16 +0200

    Python Fundamentals task

---
+++ Python-Advanced-Module/Python-OOP/03.Inheritance/Exercise/e04_nfs/project/family_car.py
@@ -0,0 +1,6 @@
+from Various_Projects.song_album_band_project import Car
+
+
+class FamilyCar(Car):
+    def __init__(self, fuel: float, horse_power: int):
+        super().__init__(fuel, horse_power)
