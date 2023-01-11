d6187c4e530bbca3c29fdae114085a76580ea3b3
stanislav-pavlov <stanislav.pavlov.94@gmail.com>
2023-01-11 14:02:56 +0200

    Python Fundamentals task

---
+++ Python-Advanced-Module/Python-OOP/03.Inheritance/Exercise/e04_nfs/project/sport_car.py
@@ -0,0 +1,9 @@
+from Various_Projects.song_album_band_project import Car
+
+
+class SportCar(Car):
+    DEFAULT_FUEL_CONSUMPTION = 10
+
+    def __init__(self, fuel: float, horse_power: int):
+        super().__init__(fuel, horse_power)
+
