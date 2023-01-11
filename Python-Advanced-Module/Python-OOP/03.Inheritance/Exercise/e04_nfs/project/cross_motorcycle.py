4a63ab894cc99158eccd49296800df248bb27a06
stanislav-pavlov <stanislav.pavlov.94@gmail.com>
2023-01-11 14:01:57 +0200

    Python Fundamentals task

---
+++ Python-Advanced-Module/Python-OOP/03.Inheritance/Exercise/e04_nfs/project/cross_motorcycle.py
@@ -0,0 +1,6 @@
+from Various_Projects.song_album_band_project import Motorcycle
+
+
+class CrossMotorcycle(Motorcycle):
+    def __init__(self, fuel: float, horse_power: int):
+        super().__init__(fuel, horse_power)
