5e949a974b832c102a7fcfa7b69e70e13699aaee
stanislav-pavlov <stanislav.pavlov.94@gmail.com>
2023-01-14 14:20:02 +0200

    Python OOP Assignments

---
+++ Python-Advanced-Module/Python-OOP/04.Encapsulation/Exercise/e01_wild_cat_zoo/project/caretaker.py
@@ -0,0 +1,6 @@
+from Various_Projects.song_album_band_project import Worker
+
+
+class Caretaker(Worker):
+    def __init__(self, name: str, age: int, salary: int):
+        super().__init__(name, age, salary)
