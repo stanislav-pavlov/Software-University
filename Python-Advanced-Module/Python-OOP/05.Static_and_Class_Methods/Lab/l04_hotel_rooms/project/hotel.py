9ac1f105cc4d3ada9ae67cbedae5273a774eebf8
stanislav-pavlov <stanislav.pavlov.94@gmail.com>
2023-01-17 14:40:09 +0200

    Python OOP Assignments

---
+++ Python-Advanced-Module/Python-OOP/05.Static_and_Class_Methods/Lab/l04_hotel_rooms/project/hotel.py
@@ -0,0 +1,31 @@
+from project.room import Room
+
+
+class Hotel:
+    def __init__(self, name):
+        self.name = name
+        self.rooms = []
+    
+    @property
+    def guests(self):
+        return sum([r.guests for r in self.rooms])
+    
+    @classmethod
+    def from_stars(cls, stars_count: int):
+        return cls(f"{stars_count} stars Hotel")
+
+    def add_room(self, room: Room):
+        self.rooms.append(room)
+
+    def take_room(self, room_number, people):
+        room = [r for r in self.rooms if r.number == room_number][0]
+        return room.take_room(people)
+
+    def free_room(self, room_number):
+        room = [r for r in self.rooms if r.number == room_number][0]
+        return room.free_room()
+
+    def status(self):
+        return f"Hotel {self.name} has {self.guests} total guests\n" \
+               f"Free rooms: {', '. join(str(r.number) for r in self.rooms if not r.is_taken)}\n" \
+               f"Taken rooms: {', '. join(str(r.number) for r in self.rooms if r.is_taken)}"
