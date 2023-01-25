1394113aeb84433437ad739dcf9a261fec9ff593
stanislav-pavlov <stanislav.pavlov.94@gmail.com>
2023-01-25 15:42:29 +0200

    Python OOP Assignments

---
+++ Python-Advanced-Module/Python-OOP/07.Iterators_and_Generators/Exercise/e06_fibonacci_generator.py
@@ -0,0 +1,21 @@
+def fibonacci():
+    first_num = 0
+    second_num = 1
+    yield first_num
+    yield second_num
+
+    while True:
+        result = first_num + second_num
+        first_num = second_num
+        second_num = result
+
+        yield result
+
+
+
+
+
+generator = fibonacci()
+for i in range(5):
+    print(next(generator))
+
