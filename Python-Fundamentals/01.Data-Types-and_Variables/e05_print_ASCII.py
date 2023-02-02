e460e871565458533dd00fb3819aaa3bc1698fc5
stanislav-pavlov <124202676+stanislav-pavlov@users.noreply.github.com>
2023-02-02 09:14:32 +0200

    Update and rename e5_print_ASCII.py to e05_print_ASCII.py

---
+++ Python-Fundamentals/01.Data-Types-and_Variables/e05_print_ASCII.py
@@ -0,0 +1,4 @@
+start_num, end_num = int(input()), int(input())
+
+for x in range(start_num, end_num+1):
+    print(chr(x), end=" ")
--- Python-Fundamentals/01.Data-Types-and_Variables/e5_print_ASCII.py
+++
@@ -1,4 +0,0 @@
-startNum, endNum = int(input()), int(input())
-
-for x in range(startNum, endNum+1):
-    print(chr(x), end=" ")
